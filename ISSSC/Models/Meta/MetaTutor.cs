588bead5eee41bf96a7cf31949ee1c20e8b1942f
Jakub <36817456+hlavja@users.noreply.github.com>
2019-03-02 22:08:48 +0100

    Tutor list - statistics

---
+++ ISSSC/Models/Meta/MetaTutor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ISSSC.Models.Meta
+{
+    public class MetaTutor
+    {
+        public int Id { get; set; }
+
+        public int Lessons { get; set; }
+        public int MathLessons { get; set; }
+        public int MechLessons { get; set; }
+        public int InfLessons { get; set; }
+
+        public int LessonsHours { get; set; }
+
+
+        public virtual SscisUser IdTutorNavigation { get; set; }
+    }
+}
